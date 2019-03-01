1171d891366fe49f722715522e9d825eb713d981
zaru <sakuraba@basicinc.jp>
2019-03-01 12:42:49 +0900

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#ff3399`,
       marginBottom: `1.45rem`,
     }}
   >
