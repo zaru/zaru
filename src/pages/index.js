9e7fa648b99f414a18cf77fec62d850f394b122f
zaru <sakuraba@basicinc.jp>
2019-12-02 15:55:18 +0900

    add profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,25 @@ const IndexPage = () => (
   <Layout>
     <SEO title="zaru" keywords={[`zaru`]} />
     <h1>zaru</h1>
+    <ul>
+      <li><a href="https://twitter.com/zaru">Twitter</a></li>
+    </ul>
+    <h2>書いたものとか、作ったもの</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/zaru">GitHub</a>
+        <ul>
+          <li><a href="https://github.com/zaru/webpush">webpush ( Ruby gem )</a></li>
+          <li><a href="https://github.com/zaru/LightningQR">LightningQR ( mac app )</a></li>
+          <li><a href="https://github.com/zaru/notify_hub">NotifyHub ( mac app )</a></li>
+          <li><a href="https://github.com/zaru/vue.tategaki">Vue tategaki ( vue component )</a></li>
+        </ul>
+      </li>
+      <li><a href="https://qiita.com/zaru">Qiita</a></li>
+      <li><a href="hhttps://tech.basicinc.jp/accounts/zaru">Basic engineer blog</a></li>
+      <li><a href="https://medium.com/@zaru">Medium</a></li>
+      <li><a href="https://www.wantedly.com/users/384807">Wantedly</a></li>
+    </ul>
   </Layout>
 )
 
